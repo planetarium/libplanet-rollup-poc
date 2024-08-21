675194aab3e897075d43586fa95c2c948b010666
Kang Seokhun <kangseokhun43@gmail.com>
2024-08-21 13:19:34 +0900

    feat: add batch data header for decompress

--- nest/batcher/models/batcher.compressor.ts
+++ nest/batcher/models/batcher.compressor.ts
@@ -8,9 +8,16 @@ export class Compressor {
     }
 
     public write(data: Uint8Array): void {
+        const dataLength = data.byteLength;
+        const dataLengthBytes = new Uint8Array(4);
+        dataLengthBytes[0] = (dataLength >> 24) & 0xff;
+        dataLengthBytes[1] = (dataLength >> 16) & 0xff;
+        dataLengthBytes[2] = (dataLength >> 8) & 0xff;
+        dataLengthBytes[3] = dataLength & 0xff;
+        data = new Uint8Array([...dataLengthBytes, ...data]); 
         this.compressed = new Uint8Array([...this.compressed, ...data]);
     }
-
+    
     public length(): number {
         return this.compressed.length;
     }
