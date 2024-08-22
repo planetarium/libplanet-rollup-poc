2583736830f2331d49f3d147f605ff8f5e11faef
Kang Seokhun <kangseokhun43@gmail.com>
2024-08-22 11:24:56 +0900

    refactor: move app controller to evm controller

--- nest/app.controller.ts
+++ nest/app.controller.ts
@@ -1,40 +1,9 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
-import { DepositEthDto } from './dto/deposit-eth.dto';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/geth')
+@Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService
   ) {}
-
-  @Get('send')
-  sendTransaction(): Promise<`0x${string}`> {
-    return this.appService.sendTransaction();
-  }
-
-  @Post('deposit')
-  async depositETH(@Body() depositEth: DepositEthDto): Promise<`0x${string}`> {
-    return this.appService.depositETH(depositEth.recipient, depositEth.amount);
-  }
-
-  @Get('propose/outputroot')
-  async proposeOutputRoot(): Promise<`0x${string}`> {
-    return this.appService.proposeOutputRoot();
-  }
-
-  @Get('prove/withdrawal')
-  async proveWithdrawal(@Query('txId') txId: string): Promise<`0x${string}`> {
-    return this.appService.proveWithdrawal(txId);
-  }
-
-  @Get('finalize/withdrawal')
-  async finalizeWithdrawal(@Query('txId') txId: string): Promise<`0x${string}`> {
-    return this.appService.finalizeWithdrawal(txId);
-  }
-
-  @Get('balance')
-  async getBalance(@Query('address') address: `0x${string}`): Promise<bigint> {
-    return this.appService.getBalance(address);
-  }
 }
--- nest/evm/evm.module.ts
+++ nest/evm/evm.module.ts
@@ -3,10 +3,11 @@ import { WalletManager } from "./wallet.client";
 import { PublicClientManager } from "./public.client";
 import { KeyManager } from "../key.utils";
 import { NCModule } from "nest/9c/nc.module";
+import { EvmService } from "./evm.service";
 
 @Module({
     imports: [NCModule],
-    providers: [WalletManager, PublicClientManager, KeyManager],
+    providers: [WalletManager, PublicClientManager, EvmService, KeyManager],
     exports: [WalletManager, PublicClientManager],
 })
 export class EvmModule {}
