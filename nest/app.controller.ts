08bbf31d1c8999c6e8d7de97662f71ac150296a7
Kang Seokhun <kangseokhun43@gmail.com>
2024-08-23 08:58:12 +0900

    feat: add web front frame ui

--- nest/app.controller.ts
+++ nest/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,4 +6,10 @@ export class AppController {
   constructor(
     private readonly appService: AppService
   ) {}
+
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'Hello world!' };
+  }
 }
